4935c187a61906ba77c61417b9a3e77b0f5e5293
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-10-11 00:03:33 +0800

    added search ingredients, and meals, and fixed response type by parsing into JSON

--- app/api/mealdb/client.rb
+++ app/api/mealdb/client.rb
@@ -4,11 +4,18 @@ module Mealdb
   class Client
     # initialize connection
     @client = Request.new
+
     # get methods
-    def self.search(ingredient)
+    def self.search_by_ingredient(ingredient)
       # clean up spaces in search string
       str = ingredient.gsub(/\s+,/, ',').gsub(/,\s+/, ',').strip
-      @client.connection.get("filter.php?i=#{str}")
+      response = @client.connection.get("filter.php?i=#{str}")
+      JSON.parse(response.body)
+    end
+
+    def self.search_by_meal(meal_id)
+      response = @client.connection.get("lookup.php?i=#{meal_id}")
+      JSON.parse(response.body)
     end
 
     def self.test
