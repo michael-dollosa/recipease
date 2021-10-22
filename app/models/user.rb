715598dc66d370d7b8724b01d62dc97ad1ac9cc4
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-10-23 02:41:28 +0800

    completed adding to newsletter automatically if user registers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :send_welcome_email
+  after_create :send_welcome_email, :subscribe_newsletter
   has_many :recipes, dependent: :destroy
   validates :email, :password, presence: true
   devise :database_authenticatable, :registerable,
@@ -11,5 +11,10 @@ class User < ApplicationRecord
 
   def send_welcome_email
     UserMailer.welcome_email(email).deliver_now
+  end
+
+  def subscribe_newsletter
+    # only proceeds when email is not yet in newsletter model
+    Newsletter.create(email: email) if Newsletter.where(email: email).empty?
   end
 end
