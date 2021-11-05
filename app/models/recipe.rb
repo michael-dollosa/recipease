07c75145e20a49330d829da09b73b761eb84a17d
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-11-05 17:47:42 +0800

    removed regex format for images

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :ingredients, dependent: :destroy
-  validates :img_url, format: { with: /\.(png|jpg|jpeg)\Z/i }
+  validates :img_url, presence: true # , format: { with: /\.(png|jpg|jpeg)\Z/i }
   validates :video_url, format: { with: %r{^(?:https?://)?(?:m\.|www\.)?(?:youtu\.be/|youtube\.com/(?:embed/|v/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$}, multiline: true }
   validates :user_id, :name, :slug, :img_url, :instructions, :video_url, presence: true
   validates :ref_id, presence: true
